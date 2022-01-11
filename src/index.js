575a0ffcbbd229dee9c2f258d4e152bcd36cbf85
Sahil Qureshi <squreshi@deqode.com>
2022-01-11 19:24:29 +0530

    module import export

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import server from './server';
+
+const port = 3000;
+server.listen(port);
+console.log(`http://localhost:${port}/`);
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import http from 'http';
 
-const port = 3000;
 const server = http.createServer((req, res) => {
   res.end('Hello World');
-}).listen(port);
-console.log(`http://localhost:${port}/`);
+});
 export default server;
