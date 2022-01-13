cf6eb03db8f7b90c6c392c4b1fac438feb2792ee
Sahil Qureshi <squreshi@deqode.com>
2022-01-13 16:20:19 +0530

    code refactor

--- src/express.js
+++ src/express.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
   if (error.isAxiosError) {
-    console.log('error :>> ', error.toJSON());
+    console.log('Axios error :>> ', error.toJSON());
     const message = error.response.data?.message || error?.statusText || error.toString();
     const status = error.response.status || 500;
     return res.status(status).send({ status, message });
